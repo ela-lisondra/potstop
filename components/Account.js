d5d48e3067d4a8fe1b77eb52377655c19ece8572
ela_lisondra <emrlisondra@gmail.com>
2021-10-03 21:57:38 +0100

    initial commit

---
+++ components/Account.js
@@ -0,0 +1,21 @@
+import EthName from './EthName'
+
+const Account = function ({ account, connect }) {
+  // TODO!!!
+  // if already logged in, it should show 
+  // the EthName component with the correct address
+  // if not logged in, show a connect button
+  // that when its clicked, will prompt us to login
+  // and store the info on the page
+  if (account) {
+    return (
+      <span> {account} </span>
+    )
+  } else {
+    return (
+      <button onClick={connect}>Connect</button>
+    )
+  }
+}
+
+export default Account;
